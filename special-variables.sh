44ffa45e7af9d5f060939b04fe74b7491b2fb034
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 11:24:36 +0530

    shellscript

--- special-variables.sh
+++ special-variables.sh
@@ -8,4 +8,4 @@ echo "home directory of current user: $HOME"
 echo "which user is running this script: $USER"
 echo "process id of current script: $$"
 sleep 70 &
-echo "process id of last command in background: $!
+echo "process id of last command in background: $!"
