2b890d1d23573e4cb043d6533a4711a6cef1caa3
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 12:19:07 +0530

    shellscript

--- install-script.sh
+++ install-script.sh
@@ -6,6 +6,21 @@ then
     echo "ERROR:: you must have sudo access to execute this script"
     exit 1 
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then  # not intalled
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing MySql ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySql ... SUCCESS"
+else 
+    echo "MySql is already... Installed"
+fi
+
 dnf install mysql -y
 if [ $? -ne 0 ]
 then 
@@ -15,12 +30,17 @@ else
     echo "Installing MySql ... SUCCESS"
 fi
 
-
-dnf install git -y
+dnf list installed git
 if [ $? -ne 0 ]
 then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
     echo "Installing GIT ... FAILURE"
     exit 1
-else 
+    else 
     echo "Installing GIT ... SUCCESS"
+    fi
+else
+    echo "git is already ... Installed"
 fi
