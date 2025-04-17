e22cd968db715a0b490c8b89e09053e635869bbc
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 10:54:45 +0530

    shellscript

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# username
+echo "please enter your username::"
+read  -s USERNAME
+echo "username entered:$USERNAME"
+# password
+echo "please enter your password::"
+read -s PASSWORD
+echo "password entered:$PASSWORD
