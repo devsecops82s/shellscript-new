8de090053a99bc5e9a219a41b1aaac86a29ef044
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 10:47:22 +0530

    shellscript

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: hello $PERSON2"
+echo "$PERSON2:: hello $PERSON1"
+echo "$PERSON1:: how are u $PERSON2"
+echo "$PERSON2 :: iam good. what about you"
+echo "$PERSON1:: nothing"
