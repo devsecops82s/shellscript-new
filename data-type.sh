aaa3ec439388214debaf8d8da094e106a8f20f15
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 11:07:57 +0530

    shellscript

--- data-type.sh
+++ data-type.sh
@@ -3,4 +3,7 @@
 # adding two integer numbers
 NUMBER1=$1
 NUMBER2=$2
+TIMESTAMP=$(date)
+echo "script executed at:$TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
+echo "SUM OF $NUMBER1 and $NUMBER2 is:$SUM"
