daf2b24fd8c73d006b51acd0c27fa0fef03534e8
srikanth <bommisettysrikanth88@gmail.com>
2025-04-17 10:42:50 +0530

    shellscript

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1 = srikanth
+PERSON2 = dinesh
+
+echo "$PERSON1 :: hello $PERSON2"
+echo "$PERSON2:: hello $PERSON1"
+echo "$PERSON1:: how are u $PERSON2"
+echo "$PERSON2 :: iam good. what about you"
+echo "$PERSON1:: nothing"
